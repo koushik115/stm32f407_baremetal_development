58e553e36b3a5643ae9d8d940cb725391fba658e
Koushik <kaushikbhat115@gmail.com>
2026-03-26 11:16:09 +0530

    Add Bare-Metal LED Blinking.
    Initial import of an STM32F407VGTX baremetal driver project and IDE configuration. Adds Eclipse project metadata (.project, .cproject, .settings), linker scripts (STM32F407VGTX_FLASH.ld, STM32F407VGTX_RAM.ld), startup assembly, headers (Inc/Drivers/Peripherals/RCC/rcc.h), source files (Src/main.c, syscalls.c, sysmem.c), and generated build artifacts under Debug (makefiles, object files, dependency files, ELF, map, list). Project configured for MCU ARM GCC managed build (Debug/Release) for STM32F407VGTx.

---
+++ stm32f407_baremetal_drivers/Inc/Drivers/Peripherals/RCC/rcc.h
@@ -0,0 +1,12 @@
+/*
+ * rcc.h
+ *
+ *  Created on: Mar 23, 2026
+ *      Author: kaush
+ */
+
+#ifndef DRIVERS_PERIPHERALS_RCC_RCC_H_
+#define DRIVERS_PERIPHERALS_RCC_RCC_H_
+
+
+#endif /* DRIVERS_PERIPHERALS_RCC_RCC_H_ */
